ad7cc8b56b97b5ab7a849db7d231ded7b2eb8ca4
Ismam Abid <151983490+iamismamabid@users.noreply.github.com>
2026-05-18 05:07:27 +0600

    Update next.config.js

--- ismamstudio/next.config.js
+++ ismamstudio/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: 'standalone', // <--- এই লাইনটি অবশ্যই যোগ করুন
 };
 
 module.exports = nextConfig;

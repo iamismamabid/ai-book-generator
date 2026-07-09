6570ceed76e4a5578c8e87eb5735a53d9fa0f8eb
iamismamabid <231001512@eastdelta.edu.bd>
2026-07-09 22:52:14 +0600

    feat: add Google Tag Manager initialization script to layout.tsx

--- ismamstudio/src/app/layout.tsx
+++ ismamstudio/src/app/layout.tsx
@@ -134,6 +134,15 @@ export default function RootLayout({
                 })
               }}
             />
+            {/* Google Tag Manager */}
+            <Script id="google-tag-manager" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TX4P8W8X');`}
+            </Script>
+
             {/* Google Analytics (gtag.js) */}
             <Script
               src="https://www.googletagmanager.com/gtag/js?id=G-B08V9NL031"

4fb5a315e93bde5be91199f5c1a934bb5d19879c
iamismamabid <231001512@eastdelta.edu.bd>
2026-08-16 02:41:47 +0600

    chore: update capacitor config and android workspace settings

--- ismamstudio/capacitor.config.ts
+++ ismamstudio/capacitor.config.ts
@@ -3,12 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.kdpage.studio',
   appName: 'KDPage Studio',
-  webDir: 'out',
-  server: {
-    // Put your live production domain here
-    url: 'https://kdpage.com',
-    cleartext: true
-  }
+  webDir: 'out'
 };
 
 export default config;
